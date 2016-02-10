932073e60ea6c619fcd54ab8dcf10133dbddf475
Michiel Overtoom <motoom@xs4all.nl>
2016-02-10 13:11:39 +0100

    Count layout events

--- PolylineBox/PolylineBox/ViewController.swift
+++ PolylineBox/PolylineBox/ViewController.swift
@@ -1,9 +1,14 @@
 import UIKit
 class ViewController: UIViewController {
+
+    @IBOutlet weak var rotationLabel: UILabel!
+
+    var nr = 0
+
+    override func viewWillLayoutSubviews() {
+        let plural = nr != 1 ? "s" : ""
+        rotationLabel.text = "\(nr) view layout event\(plural)"
+        nr += 1
+    }
+
 }
-
-// todo: viewWillLayoutSubviews() for recalc of margins etc. instead of in Draw
-
-// todo: statusBarOrientation() to determine the device orientation.
-
-// At launch time, apps should always set up their interface in a portrait orientation. After the application:didFinishLaunchingWithOptions: method returns, the app uses the view controller rotation mechanism described above to rotate the views to the appropriate orientation prior to showing the window.
