b37970eb66a05ce85db3e2439968f7ca82c26a36
Michiel Overtoom <motoom@xs4all.nl>
2016-03-25 10:41:03 +0100

    Zooming and pinching on Groningen map

--- Groningen/Groningen/ViewController.swift
+++ Groningen/Groningen/ViewController.swift
@@ -3,20 +3,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
-    @IBOutlet weak var Scroller: UIScrollView!
+    @IBOutlet weak var scroller: UIScrollView!
+    @IBOutlet weak var mapView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        Scroller.contentSize.width=5500;
-        Scroller.contentSize.height=4388;
-        Scroller.setContentOffset(CGPoint(x: 3060, y: 1280), animated: false)
+        scroller.delegate = self
+        scroller.contentSize.width=5500;
+        scroller.contentSize.height=4388;
+        scroller.setContentOffset(CGPoint(x: 3060, y: 1280), animated: false)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // Zooming
+
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        print("viewForZoomingInScrollView")
+        return mapView
+        }
+
+    func scrollViewWillBeginZooming(scrollView: UIScrollView, withView view: UIView?) {
+        print("scrollViewWillBeginZooming")
+        }
+
+    func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView?, atScale scale: CGFloat) {
+        print("scrollViewDidEndZooming")
+        }
+
+    // Scrolling
+
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        print("scrollViewDidScroll")
+        }
+
+    func scrollViewDidScrollToTop(scrollView: UIScrollView) {
+        print("scrollViewDidScrollToTop")
+        }
+
+
+    // Panning/dragging
+
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        print("scrollViewWillBeginDragging")
+        }
+
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        print("scrollViewDidEndDragging")
+        }
 }
 
