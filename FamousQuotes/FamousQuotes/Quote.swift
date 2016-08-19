dc2256fd2a26f6e59d4457364021c736c465612b
Michiel Overtoom <motoom@xs4all.nl>
2016-08-19 19:52:58 +0200

    Added Quote class

---
+++ FamousQuotes/FamousQuotes/Quote.swift
@@ -0,0 +1,14 @@
+
+//  Quote.swift
+
+import Foundation
+
+class Quote {
+    var author: String = ""
+    var quote: String = ""
+
+    init(_ author: String, _ quote: String) {
+        self.author = author
+        self.quote = quote
+        }
+    }
