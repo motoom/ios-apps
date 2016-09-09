22441b2a4e81c00b9b89cd8cdb6de9193d141709
Michiel Overtoom <motoom@xs4all.nl>
2016-09-09 13:16:38 +0200

    CSV output, accuracy filtering

---
+++ TeVoet/TeVoet/Persistence.swift
@@ -0,0 +1,58 @@
+
+//  Persistence.swift
+
+import UIKit
+import CoreLocation
+
+
+func filenametimestamp(locations: [CLLocation]) -> String {
+    let yyyymmddhhmm = NSDateFormatter()
+    yyyymmddhhmm.dateFormat = "yyyyMMddHHmm"
+    return yyyymmddhhmm.stringFromDate(locations[0].timestamp)
+    }
+
+
+func saveWaypoints(locations: [CLLocation]) {
+    // Filename voor save bepalen
+    let tijdstamp = filenametimestamp(locations)
+    let filenaam = "\(tijdstamp).v1.locations" // v1 = versie file format
+    let fullfilenaam = docdirfilenaam(filenaam)
+    // Saven.
+    // Saven als dict met keys "meta" met pedometerdata, en "locations"?
+    let data = NSKeyedArchiver.archivedDataWithRootObject(locations) // TODO: Ook pedometer data saven
+    data.writeToFile(fullfilenaam, atomically: true)
+    }
+
+
+func saveWaypointsCSV(locations: [CLLocation]) {
+    let tijdstamp = filenametimestamp(locations)
+    let filenaam = "\(tijdstamp).csv"
+    let fullfilenaam = docdirfilenaam(filenaam)
+    // CSV header
+    var csv = "\"unix timestamp\", \"datetime\", \"latitude\", \"longitude\", \"horizontal accuracy\", \"altitude\", \"vertical accuracy\"\n"
+    var cumulDistance: Double = 0, delta: Double = 0
+    var prevLocation: CLLocation?
+    for location in locations {
+        // Calculate distance and cumulative distance
+        if prevLocation ==  nil {
+            prevLocation = location
+            }
+        else {
+            delta = location.distanceFromLocation(prevLocation!)
+            cumulDistance += delta
+            prevLocation = location
+            }
+        // Format for output.
+        let regel = "\(location.timestamp.timeIntervalSince1970), \"\(location.timestamp)\", \(location.coordinate.latitude), \(location.coordinate.longitude), \(location.horizontalAccuracy), \(location.altitude), \(location.verticalAccuracy), \(delta), \(cumulDistance)\n"
+        csv += regel
+        }
+    do {
+        try csv.writeToFile(fullfilenaam,  atomically: true, encoding: NSUTF8StringEncoding)
+        }
+    catch {
+        let err = error as NSError
+        print(err.localizedDescription)
+        }
+    }
+
+
--- TeVoet/TeVoet/WandelingController.swift
+++ TeVoet/TeVoet/WandelingController.swift
@@ -7,15 +7,12 @@ import UIKit
 import MapKit
 import CoreLocation
 
-let standaardIgnoreUpdates = 3
-
 class WandelingController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
 
     var locations = [CLLocation]()
-    var locationsBuffer = [CLLocation]() // for collecting locations while in background state
-    var ignoreUpdates = standaardIgnoreUpdates // de eerste 'ignoreupdates' meldingen negeren, vanwege initiele onnauwkeurigheid
+    var locationsBackgroundBuffer = [CLLocation]() // for collecting locations while in background state
     var prevpolyline: MKPolyline? = nil
 
     var totaal = 0.0 // Actueel totaal aantal meters afgelegd.
@@ -33,7 +30,10 @@ class WandelingController: UIViewController, CLLocationManagerDelegate, MKMapVie
 
     override func viewWillDisappear(animated: Bool) {
         stopTracking()
-        saveWaypoints()
+        if locations.count >= 2 {
+            saveWaypoints(locations)
+            saveWaypointsCSV(locations)
+            }
         }
 
     // Zie ook  allowDeferredLocationUpdatesUntilTraveled:timeout: en deferredLocationUpdatesAvailable
@@ -46,38 +46,30 @@ class WandelingController: UIViewController, CLLocationManagerDelegate, MKMapVie
         if let lm = apd.lm {
             lm.delegate = self
             lm.activityType = .Fitness
-            lm.desiredAccuracy = kCLLocationAccuracyNearestTenMeters // of '-Best'
-            lm.distanceFilter = 10 // default is dit None
+            lm.desiredAccuracy = kCLLocationAccuracyNearestTenMeters // or '-Best'
+            lm.distanceFilter = 10 // default None, but then many locations arrive, even without moving
             lm.allowsBackgroundLocationUpdates = true
             lm.startUpdatingLocation()
-            ignoreUpdates = standaardIgnoreUpdates
             // print("Location updating started") // Ook te zien aan het pijltje in de statusbalk.
             }
         }
 
     func locationManager(manager: CLLocationManager, didUpdateLocations newLocations: [CLLocation]) {
-        // TODO: Dit moet beter. Bijvoorbeeld alle updates weggooien totdat ze een stabiele loopsnelheid laten zien.
-        // Of de eerste vijf seconden niets opslaan, of onnauwkeurige (>10m) locatiefixes negeren
-        if ignoreUpdates > 0 {
-            ignoreUpdates -= 1
-                // print("didUpdateLocations: Genegeerde location update:", locations)
-                return
-            }
-        // Checken of in backgroundstate. Zoja, bufferen en verder niets doen.
+        // Checken of in backgroundstate. Zoja, zo snel mogelijk bufferen en verder niets doen.
         if UIApplication.sharedApplication().applicationState == .Background {
-            locationsBuffer.appendContentsOf(newLocations)
+            locationsBackgroundBuffer.appendContentsOf(newLocations)
             // print("didUpdateLocations: \(newLocations.count) locations recorded in background buffer")
             return
             }
 
         // In foreground: eventueel eerder opgenomen locations in background buffer appenden.
-        if locationsBuffer.count > 0 {
-            locations.appendContentsOf(locationsBuffer)
+        if locationsBackgroundBuffer.count > 0 {
+            locations.appendContentsOf(locationsBackgroundBuffer.filter{$0.horizontalAccuracy < 11})
             // print("didUpdateLocations: retrieved \(locationsBuffer.count) locations from background buffer")
-            locationsBuffer.removeAll()
+            locationsBackgroundBuffer.removeAll()
             }
         // En daarna de nieuw binnenkomende locaties.
-        locations.appendContentsOf(newLocations)
+        locations.appendContentsOf(newLocations.filter{$0.horizontalAccuracy < 11})
         // print("didUpdateLocations: foreground update with \(newLocations.count) locations")
 
         // print("Aantal waypoints:", locations.count)
@@ -101,11 +93,12 @@ class WandelingController: UIViewController, CLLocationManagerDelegate, MKMapVie
                 prevLocation = location
                 dispatch_async(dispatch_get_main_queue()) {
                     let saf = sjiekeAfstand(self.totaal)
-                    self.statusLabel.text = "afgelegd: \(saf)"
+                    self.statusLabel.text = "afgelegd: \(saf)" // TODO: Om de zoveels seconden roteren: totaal afstand afgelegd, totaal aantal stappen genomen (indien pedometerdata beschikbaar), snelheid in km/hr, tijdsduur.
                     }
                 }
             }
         }
+
 
     func stopTracking() {
         if !CLLocationManager.locationServicesEnabled() {
@@ -118,29 +111,13 @@ class WandelingController: UIViewController, CLLocationManagerDelegate, MKMapVie
             }
         }
 
-    func saveWaypoints() {
-        if locations.count < 2 {
-            return
-            }
-        // Filename voor save bepalen
-        let yyyymmddhhmm = NSDateFormatter()
-        yyyymmddhhmm.dateFormat = "yyyyMMddHHmm"
-        let tijdstamp = yyyymmddhhmm.stringFromDate(locations[0].timestamp)
-        let filenaam = "\(tijdstamp).v1.locations" // v1 = versie file format
-        let fullfilenaam = docdirfilenaam(filenaam)
-        // Saven. TODO: Ook als CSV saven? Of een conversie-utility schrijven?
-        // TODO: Saven als dict met keys "meta" met pedometerdata, en "locations"?
-        let data = NSKeyedArchiver.archivedDataWithRootObject(locations) // TODO: Ook pedometer data saven
-        data.writeToFile(fullfilenaam, atomically: true)
-        }
-
 
     func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer! {
         if overlay is MKPolyline {
             let route: MKPolyline = overlay as! MKPolyline
             let routeRenderer = MKPolylineRenderer(polyline:route)
             routeRenderer.lineWidth = 5.0
-            routeRenderer.strokeColor = UIColor.redColor() // UIColor(red: 240.0/255.0, green: 68.0/255.0, blue: 0.0/255.0, alpha: 1);
+            routeRenderer.strokeColor = UIColor.redColor()
             return routeRenderer
             }
         return nil
